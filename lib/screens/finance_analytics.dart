1b4c5f53122fbe9cb1eaec2f4d42bf6a61d79b0d
Zaytsev24 <army_24@mail.ru>
2026-01-11 18:03:27 +0500

    Create finance_analytics.dart

---
+++ lib/screens/finance_analytics.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FinanceAnalyticsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Finance & Analytics')));
+  }
+}
